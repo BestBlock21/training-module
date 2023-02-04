5cb47838d2c48372c93c3937aedf127fead15e61
kitchenglora <kitchengloraapp@gmail.com>
2023-02-04 17:01:54 +0530

    first commit

--- pages/module1/EmployeeHandbook.js
+++ pages/module1/EmployeeHandbook.js
@@ -3,18 +3,14 @@ import React, { useState } from 'react'
 import styles from '@/styles/Home.module.css'
 import Head from 'next/head'
 // import Card from '@/components/card'
-import { CSSTransition } from 'react-transition-group'
+// import { CSSTransition } from 'react-transition-group'
 const employeeHandbook = () => {
   // const [ShowFront,SetShowFront] = useState(false)
   return (
     <>
  <div className= {styles.app}>
 <div className= {styles.flippableCardContainer}>
-  <CSSTransition 
-  timeout = {300} 
- >
-  {/* <Card /> */}
-  </CSSTransition>
+  
 
 </div>
 </div> 
