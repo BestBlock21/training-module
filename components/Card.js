a61d34c8189e83ac75a31158ee7e33b069ab9ad0
kitchenglora <kitchengloraapp@gmail.com>
2023-02-04 16:28:12 +0530

    second

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styles from '@/styles/Home.module.css'
 // import styles from '@/styles/flipCard.css'
-const Card = ({onClick}) => {
+const Card = () => {
   return (
-    <div className= {styles.card} onClick = {onClick} >
+    <div className= {styles.card}  >
         <div  className= {styles.cardFront}>Front</div>
     <div className= {styles.cardBack}>Back</div>
     
--- pages/module1/EmployeeHandbook.js
+++ pages/module1/EmployeeHandbook.js
@@ -5,17 +5,15 @@ import Head from 'next/head'
 import Card from '@/components/card'
 import { CSSTransition } from 'react-transition-group'
 const employeeHandbook = () => {
-  const [ShowFront,SetShowFront] = useState(false)
+  // const [ShowFront,SetShowFront] = useState(false)
   return (
     <>
  <div className= {styles.app}>
 <div className= {styles.flippableCardContainer}>
-  <CSSTransition in = {ShowFront}
+  <CSSTransition 
   timeout = {300} 
   classNames = 'flip'>
-  <Card onClick={()=>{
-SetShowFront((v)=>!v);
-  }} />
+  <Card />
   </CSSTransition>
 
 </div>
